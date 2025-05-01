8ff95b2e3d0d3b459f3e366d0716061c5c3bbda6
Shubham-60 <workshubham38@gmail.com>
2025-05-02 01:23:05 +0530

    Navbar Added

---
+++ app/components/Header/Header.js
@@ -0,0 +1,34 @@
+"use client";
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState } from 'react';
+import "./style.css";
+
+export default function Header() {
+  const [isGateOpen, setIsGateOpen] = useState(false);
+  
+  const toggleGate = () => {
+    setIsGateOpen(!isGateOpen);
+  };
+  return (
+    <header className="header">
+      <div className="header-container">
+        <Link href="/" className="title">
+          GetSetGo
+        </Link>
+        <nav className="nav">
+          <Link href="/">Home</Link>
+          <Link href="/explore">Explore</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+        </nav>
+        <div className="auth-buttons">
+          <button className="login-button" onClick={toggleGate}>
+            {isGateOpen ? 'Logout' : 'Login'}
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
