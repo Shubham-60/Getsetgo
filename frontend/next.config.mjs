97eda4572694e07946832aca404a634dab3d66cd
Shubham-60 <workshubham38@gmail.com>
2026-01-29 11:19:12 +0530

    fix Next.js version

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,22 +1,4 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    authInterception: true,
-  },
-  httpAgentOptions: {
-    keepAlive: true,
-  },
-};
-
-export const middleware = clerkMiddleware();
-
-export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
-};
+const nextConfig = {};
 
 export default nextConfig;
